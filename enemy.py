a7b2cfc23cfef7e278b69f9e766412dc5d39454e
sendonaris <elinas@mit.edu>
2019-11-19 03:29:47 -0500

    Added note animations

--- enemy.py
+++ enemy.py
@@ -10,6 +10,10 @@ import numpy as np
 
 PROJECTILE_SPEED = 10
 ENEMY_SPEED = 10
+# this is the amount of time during which the note bounces in sec
+BOUNCE_TIME = 0.15
+# maximum bounce height as a function of tile size
+MAX_BOUNCE_HEIGHT = 0.5
 
 class Enemy(Entity):
     def __init__(self, desc, action_description, map, is_enemy_pacified):
@@ -33,9 +37,12 @@ class Enemy(Entity):
 
         pixel_pos = self.map.tile_to_pixels(self.pos)
 
-        # TODO: change this! self.graphic should have an on_update(), and Rectangle doesn't.
-        # Because of this, self.on_update() doesn't call super's on_update()
-        self.graphic = EnemyGraphic(self.pos, desc["sprite"], map)
+        self.sprites = desc["sprites"]
+        if self.is_enemy_pacified(self.id):
+            sprite = desc["sprites"]["pacified"]
+        else:
+            sprite = desc["sprites"]["angry"]
+        self.graphic = EnemyGraphic(self.pos, sprite, map)
         self.draw_graphics()
         self.projectiles = AnimGroup()
         self.add(self.projectiles)
@@ -45,26 +52,14 @@ class Enemy(Entity):
         next_attack = self.actions.get_next_attack()
         if (not self.is_enemy_pacified(self.id)) and next_attack != '':
             p_pos = np.array(self.pos)
-            return Projectile(p_pos, next_attack, self.map)
+            return Projectile(p_pos, next_attack, self.map, self.sprites["projectile"])
         return None
 
-    # TODO: returns a list of Projectile objects created in this round
     def on_beat(self, map, music, movement):
         # move the enemy
         self.pos = self.actions.get_next_pos(self.pos)
         # self.graphic.pos = self.map.tile_to_pixels(self.pos)
         self.graphic.set_position(self.pos)
-
-        # make the next projectile
-        # projectile = None
-        # next_attack = self.actions.get_next_attack()
-        # if next_attack != '':
-        #     p_pos = np.array(self.pos)
-        #     self.projectiles.add(Projectile(p_pos, next_attack, self.map))
-            # self.graphic.add(Projectile(p_pos, next_attack, self.map))
-            # self.add(self.projectiles[-1])
-            # print(p_pos)
-            # projectile = Projectile(p_pos, next_attack, self.map)
 
         projectile = self.get_projectile()
         if projectile != None:
@@ -73,29 +68,9 @@ class Enemy(Entity):
         for p in self.projectiles.objects:
             p.on_beat(map, music, movement)
 
-        # add them to the map so it knows where they are
+        # add the enemy to the map so it knows where they are
         # using its add_enemy(self, position, enemy):
         map.add_enemy(self.pos, self)
-
-        # p_kill_list = []
-        # print(self.graphic.objects)
-        # for p in self.projectiles.objects:
-            # update the position of the projectile
-            # next_pos = p.get_next_pos()
-            # get rid of projectiles that have gone off the edge of the screen
-            # print(map.map_size())
-            # print(next_pos)
-            # if next_pos[0] < 0 or next_pos[1] < 0 or next_pos[1] > map.map_size()[0] or next_pos[0] > map.map_size()[1]:
-            #     p_kill_list.append(p)
-            # # otherwise, add them
-            # else:
-            # The projectile should be able to remove itself at the end of its lifetime
-            # p.on_beat(map, music, movement)
-            # map.add_enemy(p.pos, p)
-
-        # for p in p_kill_list:
-            # self.projectiles.remove(p)
-            # self.remove(p)
 
     def on_update(self, dt=None):
         self.projectiles.on_update()
@@ -118,6 +93,9 @@ class EnemyGraphic(EntityGraphic):
     def set_position(self, new_pos):
         self.next_pos = new_pos
 
+    def set_sprite(self, sprite):
+        self.sprite = sprite
+
     def on_update(self, dt=None):
         dt = kivyClock.frametime
 
@@ -132,7 +110,6 @@ class EnemyGraphic(EntityGraphic):
 
         self.rect.pos = self.map.tile_to_pixels(self.pos)
 
-        # return self.pos[0] > 0 and self.pos[1] > 0 and self.pos[1] > self.map.map_size()[0] and self.pos[0] > self.map.map_size()[1]
         return True
 
 # map a direction character to a deltax and deltay
@@ -143,9 +120,10 @@ direction_map = {
     'r': (0, 1)
 }
 
+
 # The class that keeps track of projectiles and their positions, and moves them
 class Projectile(Entity):
-    def __init__(self, init_pos, dir, map):
+    def __init__(self, init_pos, dir, map, sprite):
         super(Projectile, self).__init__()
         self.pos = np.array(init_pos)
         self.next_pos = self.pos
@@ -153,35 +131,48 @@ class Projectile(Entity):
         self.dir = dir
         self.map = map
 
-        # self.graphic = ProjectileGraphic(init_pos, map)
+        self.graphic = ProjectileGraphic(init_pos, map)
         # self.draw_graphics()
+        # self.add(self.graphic)
         pixel_pos = self.map.tile_to_pixels(self.pos)
         self.pixel_size = np.array(map.tile_size())*0.5
 
-        self.add(Color(0,0.8,0.8))
-        self.rect = Rectangle(pos=pixel_pos + self.pixel_size / 2, size=self.pixel_size)
-        # self.rect = Rectangle(pos=(10, 10), size=(100, 100), source='./data/sprite/crappy_enemy.PNG')
+        # self.add(Color(0,0.8,0.8))
+        self.rect = Rectangle(pos=pixel_pos, size=map.tile_size(), source=sprite)
+
+        # self.rect = Rectangle(pos=pixel_pos + self.pixel_size / 2, size=self.pixel_size, source=sprite)
         self.add(self.rect)
 
+        # keep track of where we are in the bounce
+        self.bouncing = False
+        self.bounce_prog = 0
 
     # returns the next position
     def get_next_pos(self):
-        # self.pos += direction_map[self.dir]
-        # self.graphic.set_next_pos(self.pos)
         return self.pos + direction_map[self.dir]
-
-
 
     def set_next_pos(self, next_pos):
         self.next_pos = next_pos
 
+    # give the deltay based on the progress of the bounce
+    def get_deltay_bounce(self):
+        return 2 * self.bounce_prog ** 2 - 2 * self.bounce_prog
+
     def on_beat(self, map, music, movement):
         next_pos = self.get_next_pos()
         self.set_next_pos(next_pos)
+        self.bouncing = True
+        self.bounce_prog = 0
         map.add_enemy(next_pos, self)
 
     def on_update(self, dt=None):
         dt = kivyClock.frametime
+        self.bounce_prog += dt / BOUNCE_TIME
+        if self.bounce_prog > 1:
+            self.bouncing = False
+            deltay = 0
+        else:
+            deltay = self.get_deltay_bounce()
 
         disp = self.next_pos - self.pos
         dist = np.linalg.norm(disp)
@@ -192,16 +183,10 @@ class Projectile(Entity):
             delta = disp * dt * PROJECTILE_SPEED / dist
             self.pos = self.pos + delta
 
-        self.rect.pos = self.map.tile_to_pixels(self.pos) + self.pixel_size / 2
+        # self.rect.pos = self.map.tile_to_pixels(self.pos) + self.pixel_size / 2
+        self.rect.pos = self.map.tile_to_pixels(self.pos + [deltay, 0])
 
         return self.pos[0] > 0 and self.pos[1] > 0 and self.pos[1] < self.map.map_size()[1] and self.pos[0] < self.map.map_size()[0]
-
-    # dummy function because enemy group calls this for all enemies, of which
-    # projectiles are one type
-    def get_projectile(self):
-        pass
-
-    # we can use the default on_update because it's just updating the graphic
 
 
 class ProjectileGraphic(EntityGraphic):
@@ -214,7 +199,7 @@ class ProjectileGraphic(EntityGraphic):
         pixel_pos = self.map.tile_to_pixels(self.pos)
         self.pixel_size = np.array(map.tile_size())*0.5
 
-        self.add(Color(0,0.8,0.8))
+        # self.add(Color(0,0.8,0.8))
         self.rect = Rectangle(pos=pixel_pos + self.pixel_size / 2, size=self.pixel_size)
         # self.rect = Rectangle(pos=(10, 10), size=(100, 100), source='./data/sprite/crappy_enemy.PNG')
         self.add(self.rect)
